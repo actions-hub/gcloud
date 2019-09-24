809ff79bb77ea80c95e2f45d4f0f7232b82dc7da
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2019-09-24 23:42:50 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,22 @@
 
 set -e
 
-if [ -z "${APPLICATION_CREDENTIALS-}" ]; then
-   echo "APPLICATION_CREDENTIALS not found. Exiting...."
-   exit 1
+if [ ! -d "$HOME/.config/gcloud" ]; then
+   if [ -z "${APPLICATION_CREDENTIALS-}" ]; then
+      echo "APPLICATION_CREDENTIALS not found. Exiting...."
+      exit 1
+   fi
+
+   if [ -z "${PROJECT_ID-}" ]; then
+      echo "PROJECT_ID not found. Exiting...."
+      exit 1
+   fi
+
+   echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
+
+   gcloud auth activate-service-account --key-file=/tmp/account.json
+   gcloud config set project "$PROJECT_ID"
 fi
-
-if [ -z "${PROJECT_ID-}" ]; then
-   echo "PROJECT_ID not found. Exiting...."
-   exit 1
-fi
-
-echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
-
-gcloud auth activate-service-account --key-file=/tmp/account.json
-gcloud config set project "$PROJECT_ID"
 
 echo ::add-path::/google-cloud-sdk/bin/gcloud
 
