f67e01529c17875597a712c40c3878aa74d94181
keithconvictional <keith@convictional.com>
2020-05-27 09:41:51 -0400

    Updated to support backwards capability and using inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,39 @@
 set -e
 
 
-if [ -z "${APPLICATION_CREDENTIALS-}" ]; then
+if [[ -z "$APPLICATION_CREDENTIALS" ] && [ -z "$INPUT_APPLICATION_CREDENTIALS" ]]
+then
   echo "APPLICATION_CREDENTIALS not found. Exiting...."
   exit 1
-else
-  echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
-  gcloud auth activate-service-account --key-file=/tmp/account.json
 fi
 
-if [ -z "${PROJECT_ID-}" ]; then
+# This allows for backwards capability. The old way is setting the env, but this
+# handles env not set use input.
+if [ -z $APPLICATION_CREDENTIALS ]
+then
+  APPLICATION_CREDENTIALS=$INPUT_APPLICATION_CREDENTIALS
+fi
+
+# Credentials are provided so they will be used.
+echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
+gcloud auth activate-service-account --key-file=/tmp/account.json
+
+# Either the env.PROJECT_ID needs to be set or input.PROJECT_ID.
+if [[ -z "$PROJECT_ID" ] && [ -z "$INPUT_PROJECT_ID" ]]
+then
   echo "PROJECT_ID not found. Exiting...."
   exit 1
-else
-  gcloud config set project "$PROJECT_ID"
 fi
+
+# This allows for backwards capability. The old way is setting the env, but this
+# handles env not set use input.
+if [ -z $PROJECT_ID ]
+then
+  PROJECT_ID=$INPUT_PROJECT_ID
+fi
+
+# Set the project id
+gcloud config set project "$PROJECT_ID"
 
 echo ::add-path::/google-cloud-sdk/bin/gcloud
 echo ::add-path::/google-cloud-sdk/bin/gsutil
